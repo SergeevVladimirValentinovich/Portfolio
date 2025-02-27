7ea30885da372baa8e4f424951801bc325c0935f
Vladimir <145487820+SergeevVladimirValentinovich@users.noreply.github.com>
2025-02-27 18:09:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let progressBars = document.querySelectorAll(".progress");
-
-    progressBars.forEach(bar => {
-        let value = bar.getAttribute("data-value");
-        bar.style.width = value + "%";
-    });
+    console.log("Портфолио загружено!");
 });
